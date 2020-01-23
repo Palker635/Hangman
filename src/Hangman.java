342ec4588ce831d9a2906ee5ecf8a9041c75e5c9
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2020-01-23 11:13:51 +0100

    192846124

--- src/Hangman.java
+++ src/Hangman.java
@@ -17,6 +17,10 @@ public class Hangman {
         System.out.println("Your choice is: " + choice);
 
         if (choice == "friend") {
+        System.out.println("Let 1 player choose a word and then let the other player begin guessing letters.");
+
+        System.out.println("Write down your guess");
+        Scanner guess = new Scanner(System.in);
 
         }
         else if (choice == "bot") {
@@ -24,16 +28,14 @@ public class Hangman {
         }
 
         else {
-            System.out.println("Welcome to Hangman!");
-            System.out.println("Do you wanna play with a friend or against a bot?");
-            System.out.println("Tips: write 'friend' or 'bot' below");
-
-            Scanner test = new Scanner(System.in);
+            System.out.println("Error, you have to write either bot or friend");
+            test = new Scanner(System.in);
             System.out.println("Do you wanna play with a friend or against a bot? Enter your choice below");
 
-            String choice = test.nextLine();
+            choice = test.nextLine();
             System.out.println("Your choice is: " + choice);
-        }
 
+
+        }
     }
 }
